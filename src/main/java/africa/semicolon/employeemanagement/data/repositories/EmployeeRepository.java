a2f4594db150a0c0daa42c65e279cc3241027035
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2022-09-04 00:59:53 +0100

    updated

--- src/main/java/africa/semicolon/employeemanagement/data/repositories/EmployeeRepository.java
+++ src/main/java/africa/semicolon/employeemanagement/data/repositories/EmployeeRepository.java
@@ -2,16 +2,15 @@ package africa.semicolon.employeemanagement.data.repositories;
 
 import africa.semicolon.employeemanagement.data.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
 @Repository
-//@EnableJpaRepositories
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    boolean existsByEmail(String email);
+
 
     Optional <Employee> findByEmail(String email);
 
